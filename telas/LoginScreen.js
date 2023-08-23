4c5832ba69205b7734e43e6d607aef1c2286dad3
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-23 13:50:05 -0300

    criação de telas

---
+++ telas/LoginScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    navigation.navigate('Cardapio');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Nome de Usuário:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <Text style={styles.label}>Senha:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  label: {
+    fontSize: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
+
+export default LoginScreen;
