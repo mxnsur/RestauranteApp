0ed8717f32c8affe2308ed58760cd51207734ca5
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-28 17:28:40 -0300

    importando routes

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes/Routes';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 	return (
