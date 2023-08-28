0cedc622c1f5909b80a4e510dc70f41a96690582
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-28 17:29:12 -0300

    expo status bar

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes/Routes';
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 
 export default function App() {
 	return (
