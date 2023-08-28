32b9246fd59eb4bba0eb101880b85054712d0bb1
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-28 17:25:42 -0300

    removendo imports

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/routes/Routes';
-import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 	return (
